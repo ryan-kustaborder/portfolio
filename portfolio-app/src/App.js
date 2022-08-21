1fcaeec76716bfa3151053c102395069b3bce419
Ryan Kustaborder <rkusty23@gmail.com>
2022-08-21 15:14:21 -0400

    Acceptable Home Page

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,7 +1,10 @@
 import ProjectLink from "./ProjectLink";
 
 import ACC_MAT from "./images/accMat.png";
+import CHESS from "./images/chess.png";
 import HEADSHOT from "./images/Headshot.png";
+
+import RESUME from "./images/resume.pdf";
 
 import GithubLogo from "./images/GithubLogo";
 import EmailIcon from "./images/EmailIcon";
@@ -31,7 +34,7 @@ export default function App() {
                 <div className="content grid">
                     <div className="span--5">
                         <h1>Welcome</h1>
-                        <div>
+                        <div className="section-description">
                             <h3>I am Ryan Kustaborder</h3>
                             <p>
                                 and I am a front-end developer who emphasizes
@@ -42,10 +45,10 @@ export default function App() {
                                 >
                                     accessibility and usability.
                                 </a>
-                                Morbi posuere purus dolor, nec cursus lacus
-                                semper nec. Etiam venenatis elit sem. Praesent
-                                facilisis lectus sed feugiat venenatis.
                             </p>
+                            <a href={RESUME} target="_blank" rel="noreferrer">
+                                View my Resume
+                            </a>
                         </div>
                     </div>
 
@@ -66,41 +69,29 @@ export default function App() {
                         href="http://eecs.csuohio.edu/~rykustab/project1/index.html"
                     />
                     <ProjectLink
-                        src={ACC_MAT}
-                        title="Accessibility Matters."
-                        alt="Screenshot of my Accessibility Matters Project"
-                        desc="This is a project I did for ART 347 at Cleveland State University"
-                        href="http://eecs.csuohio.edu/~rykustab/project1/index.html"
-                    />
-                    <ProjectLink
-                        src={ACC_MAT}
-                        title="Accessibility Matters."
-                        alt="Screenshot of my Accessibility Matters Project"
-                        desc="This is a project I did for ART 347 at Cleveland State University"
-                        href="http://eecs.csuohio.edu/~rykustab/project1/index.html"
+                        src={CHESS}
+                        title="Chess Algorithm"
+                        alt="Screenshot of my Chess Project"
+                        desc="I developed an in-browser chess application that included a basic computer opponent based on the minimax algorithm."
+                        href="https://ryan-kustaborder.github.io/cis434s22g5/"
                     />
                 </div>
             </div>
 
             <div id="Generative" className="section noisy">
                 <div className="content grid">
-                    <h2 className="span--9">Generative Art</h2>
-                    <div className="section-description span--5">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Maecenas ornare vehicula malesuada. Quisque
-                            blandit condimentum nunc, eget aliquam nulla. Sed
-                            vitae turpis non magna ullamcorper dapibus ut
-                            sodales nunc. Aenean pretium maximus ipsum nec
-                            euismod. Duis mattis, dui eu molestie convallis,
-                            nunc nulla malesuada ex, id egestas lorem purus ac
-                            quam. Aliquam molestie efficitur mi, et blandit odio
-                            vulputate sed. Quisque consectetur mi sed imperdiet
-                            varius. Donec auctor et erat a porta. Phasellus in
-                            erat varius, tincidunt mauris vel, pellentesque
-                            velit.
-                        </p>
-                        <a href="./">Visit the Gallery</a>
+                    <div className="span--5">
+                        <h2>Generative Art</h2>
+                        <div className="section-description">
+                            <p>
+                                The ability to create complex and interesting
+                                systems and designs from simple logic is
+                                something that has always fascinated me, and
+                                Generative Art is a shining example of this
+                                phenomenon.
+                            </p>
+                            <a href="./">Visit the Gallery</a>
+                        </div>
                     </div>
                     <div className="generative-gallery span--4">
                         <div></div>
@@ -110,23 +101,18 @@ export default function App() {
 
             <div id="Design" className="section noisy">
                 <div className="content grid">
-                    <h2 className="span--9">Graphic Design</h2>
-                    <div className="section-description span--5">
-                        <p className="span--5">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Maecenas ornare vehicula malesuada. Quisque
-                            blandit condimentum nunc, eget aliquam nulla. Sed
-                            vitae turpis non magna ullamcorper dapibus ut
-                            sodales nunc. Aenean pretium maximus ipsum nec
-                            euismod. Duis mattis, dui eu molestie convallis,
-                            nunc nulla malesuada ex, id egestas lorem purus ac
-                            quam. Aliquam molestie efficitur mi, et blandit odio
-                            vulputate sed. Quisque consectetur mi sed imperdiet
-                            varius. Donec auctor et erat a porta. Phasellus in
-                            erat varius, tincidunt mauris vel, pellentesque
-                            velit.
-                        </p>
-                        <a href="./">Visit the Gallery</a>
+                    <div className="span--5">
+                        <h2 className="span--9">Graphic Design</h2>
+                        <div className="section-description">
+                            <p className="span--5">
+                                I am also a graphic designer who was
+                                particularly influenced by the Swiss
+                                International Style. I try to carry these design
+                                principles throughout all of my work, no matter
+                                the medium.
+                            </p>
+                            <a href="./">Visit the Gallery</a>
+                        </div>
                     </div>
                     <div className="design-gallery span--4">
                         <div></div>
