f04d2885165071aa29339c7f47711b4fdbf62b43
Ryan Kustaborder <rkusty23@gmail.com>
2022-08-20 23:24:14 -0400

    Base layout complete

--- portfolio-app/src/ProjectLink.jsx
+++ portfolio-app/src/ProjectLink.jsx
@@ -12,10 +12,7 @@ class ProjectLink extends Component {
                 <div className="project-description">
                     <h4>{this.props.title}</h4>
                     <p>{this.props.desc}</p>
-                    <a
-                        href="http://eecs.csuohio.edu/~rykustab/project1/index.html"
-                        target="_blank"
-                    >
+                    <a href={this.props.href} target="_blank" rel="noreferrer">
                         View Project
                     </a>
                 </div>
